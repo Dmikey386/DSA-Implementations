471a471cb0311079028479afa2744e03e19206ad
Michael Dodd <dodd.m@northeatern.edu>
2025-03-20 16:13:33 -0400

    Cleaned up linked list comments and bugs

--- LinearDataStructures/LinkedList/SinglyLinkedList.java
+++ LinearDataStructures/LinkedList/SinglyLinkedList.java
@@ -1,38 +1,37 @@
 package LinearDataStructures.LinkedList;
 
-import java.util.LinkedList;
-
 public class SinglyLinkedList<T> {
-    Node head;
-    Node tail;
+    Node<T> head;
+    Node<T> tail;
     int length;
 
     // Linked list constructor
     public SinglyLinkedList(T data) {
-        head = new Node(data);
+        head = new Node<>(data);
         tail = head;
         length = 1;
     }
 
-    // static class node
-    static class Node<T> {
+    // static class Node
+    private static class Node<T> {
         T data;
-        Node next;
+        Node<T> next;
 
-        // node class constructor
-        public Node(T data) {
+        // Node class constructor
+        public Node (T data) {
             this.data = data;
-            next = null;
+            this.next = null;
         }
 
     }
 
     public void append(T data) {
-        // append to end of linked list
-        Node newNode = new Node(data);
+        // append to end of linked list O(1)
+        Node<T> newNode = new Node<>(data);
         if (tail == null) {  // Check if the list is empty
             head = newNode;
             tail = newNode;
+            length = 1;
 
         } else {
             tail.next = newNode;
@@ -42,11 +41,12 @@ public class SinglyLinkedList<T> {
     }
 
     public void prepend(T data) {
-        // append to front of linked list
-        Node newNode = new Node(data);
+        // append to front of linked list O(1)
+        Node<T> newNode = new Node<>(data);
         if (head == null) {
             head = newNode;
             tail = newNode;
+            length = 1;
         }
         else {
             newNode.next = head;
@@ -56,7 +56,7 @@ public class SinglyLinkedList<T> {
     }
 
     public void insert(int index, T data) {
-        // insert at desired position of linked list
+        // insert at desired position of linked list O(n)
         if (index > length || index < 0) {
             throw new IndexOutOfBoundsException();
         }
@@ -67,9 +67,9 @@ public class SinglyLinkedList<T> {
             append(data);
         }
         else {
-            Node newNode = new Node(data);
-            Node beforeNode = traversal(index-1);
-            Node afterNode = beforeNode.next;
+            Node<T> newNode = new Node<>(data);
+            Node<T> beforeNode = traversal(index-1);
+            Node<T> afterNode = beforeNode.next;
             beforeNode.next = newNode;
             newNode.next = afterNode;
             length++;
@@ -78,60 +78,73 @@ public class SinglyLinkedList<T> {
     }
 
     public T removeFront(){
-        // remove from front of linked list
-        T data = (T) head.data;
+        if (head == null) { return null; }
+        // remove from front of linked list O(1)
+        T data = head.data;
         head = head.next;
         length--;
         return data;
     }
 
-    public T removeEnd(){
-        // remove from the end of the linked list
-        T data = (T) tail.data;
-        if (length == 1){
-            head = null;
-            tail = head;
+    public T removeEnd() {
+        // remove from end of linked list O(n)
+        if (head == null) {
+            return null;
         }
-        else{
 
-            tail = traversal(length-1-1);
-            tail.next = null;
+        T data = tail.data;
+
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            Node<T> newTail = traversal(length - 2);
+            newTail.next = null;
+            tail = newTail;
         }
         length--;
         return data;
     }
 
+
     public T remove(int index){
+        // remove from desired index O(n)
         if (index >= length || index < 0) {
             throw new IndexOutOfBoundsException();
         }
         if (index == 0){
             return removeFront();
         }
-        // remove from desired index of linked list
-        Node beforeIndexNode = traversal(index-1);
-        Node removeNode = beforeIndexNode.next;
-        T data = (T) removeNode.data;
-        Node afterIndexNode = removeNode.next;
-        beforeIndexNode.next = afterIndexNode;
+
+        Node<T> beforeIndexNode = traversal(index-1);
+        Node<T> removeNode = beforeIndexNode.next;
+        T data = removeNode.data;
+        beforeIndexNode.next = removeNode.next;
         length--;
         return data;
     }
 
-    public Node traversal(int index){
-        // traverse through linked list
-        if (index > length - 1){
+    public Node<T> traversal(int index){
+        // traverse through linked list O(n)
+        if (index >= length){
             throw new IndexOutOfBoundsException("Index out of bounds");
         }
-        Node traversal = head;
+        Node<T> traversal = head;
         for (int i = 0; i < index; i++) {
             traversal = traversal.next;
         }
         return traversal;
     }
 
+    public T get(int index){
+        // get data at desired index O(n)
+        Node<T> traversal = traversal(index);
+        return traversal.data;
+    }
+
+    // print linked list items for testing
     public void printLinkedList() {
-        Node traversal = head;
+        Node<T> traversal = head;
         System.out.print("[");
         while (traversal != null) {
             System.out.print(traversal.data + " --> ");
@@ -152,19 +165,24 @@ public class SinglyLinkedList<T> {
         myLinkedlist.prepend(12);
         myLinkedlist.printLinkedList();
         myLinkedlist.insert(2,20);
+        myLinkedlist.printLinkedList();
         myLinkedlist.insert(3,21);
         myLinkedlist.printLinkedList();
         myLinkedlist.removeFront();
         myLinkedlist.printLinkedList();
         myLinkedlist.removeEnd();
         myLinkedlist.printLinkedList();
-        myLinkedlist.removeFront();
-
-//[10 --> 1 --> 2 --> 3 --> ]
+        myLinkedlist.remove(4);
+        myLinkedlist.printLinkedList();
+        System.out.println(myLinkedlist.get(3));
+//    [10 --> 1 --> 2 --> 3 --> ]
 //[12 --> 11 --> 10 --> 1 --> 2 --> 3 --> ]
+//[12 --> 11 --> 20 --> 10 --> 1 --> 2 --> 3 --> ]
 //[12 --> 11 --> 20 --> 21 --> 10 --> 1 --> 2 --> 3 --> ]
 //[11 --> 20 --> 21 --> 10 --> 1 --> 2 --> 3 --> ]
-//[11 --> 20 --> 21 --> 10 --> 1 --> 2 --> ]
+//[11 --> 20 --> 21 --> 10 --> 1 --> 2 --> 3 --> ]
+//[11 --> 20 --> 21 --> 10 --> 2 --> 3 --> ]
+//       10
 
     }
 }
